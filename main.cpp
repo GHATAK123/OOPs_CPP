25d81f5f3bca210724a214289160e0f90e9929cc
PRAKASH ANAND <34940322+GHATAK123@users.noreply.github.com>
2020-01-23 06:35:11 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class HumanBeing  {
+	public :
+		void display()
+		{
+			cout << "Hii, I'm ghatak";
+		}
+	
+};
+
+int main() {
+	
+	HumanBeing H1;
+	H1.display();
+	
+	return 0;
+}
